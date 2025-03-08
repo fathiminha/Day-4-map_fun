90f3cfa60135dfacbe5ab917c6c7c6d08bd9496b
Fathima Minha <minhasulaikar@gmail.com>
2025-03-08 18:36:37 +0530

    created some student objects

--- Day4/src/main/java/com/example/demo/Controller/Myapp.java
+++ Day4/src/main/java/com/example/demo/Controller/Myapp.java
@@ -1,5 +1,21 @@
 package com.example.demo.Controller;
 
-public class Myapp {
+import com.example.demo.model.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
+public class Myapp {
+	Student Bob = new Student("2021IT01", "Bob Marely", 23, "IT", 3.2);
+	Student Alice = new Student("2021IT02", "Alice corner", 24, "IT", 3.6);
+	Student John = new Student("2021IT03", "John Parker", 23, "IT", 3.3);
+	Student Jeorge = new Student("2020IT04", "Joerge Micheal", 25, "IT", 3.4);
 }
+
