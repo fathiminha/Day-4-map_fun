bc713fed782be7249e25dd35e8054372909c6270
Fathima Minha <minhasulaikar@gmail.com>
2025-03-08 18:32:55 +0530

    student model is created

--- Day4/src/main/java/com/example/demo/model/Student.java
+++ Day4/src/main/java/com/example/demo/model/Student.java
@@ -6,4 +6,57 @@ public class Student {
 	private int age;
 	private String course;
 	private double gpa;
+	
+	public Student(String name, String regNo, int age, String course, double gpa) {
+		super();
+		this.name = name;
+		this.regNo = regNo;
+		this.age = age;
+		this.course = course;
+		this.gpa = gpa;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRegNo() {
+		return regNo;
+	}
+
+	public void setRegNo(String regNo) {
+		this.regNo = regNo;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+	public double getGpa() {
+		return gpa;
+	}
+
+	public void setGpa(double gpa) {
+		this.gpa = gpa;
+	}
+	
+	
+	
+	
 }
